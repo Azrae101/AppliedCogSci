846516d7056348865671aff7e9a00088fe8a6b99
Clara <clar.holst@gmail.com>
2024-03-06 09:37:48 +0100

    Endless mode

--- Before_they_fall.py
+++ Before_they_fall.py
@@ -16,7 +16,7 @@ list_of_answers = ["1", "2", "3", "4"]
 # Create a Tkinter application window
 window = tk.Tk()
 window.title("Before they fall")
-window.geometry("350x650")  # Increased height to accommodate the entry field
+window.geometry("350x650") 
 
 # Entry widget to take user input
 entry = tk.Entry(window, font=("Arial", 12))
@@ -24,7 +24,7 @@ entry.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
 entry.focus_set()
 
 # Create a canvas to draw animation
-canvas = tk.Canvas(window, bg="white", width=300, height=500)  # Increased canvas height
+canvas = tk.Canvas(window, bg="white", width=300, height=500) 
 canvas.grid(row=1, column=0, padx=20, pady=20)
 
 # Point counter label
@@ -41,39 +41,45 @@ def update_point_counter():
 # Function to update the label text and animate
 def update_label():
     global label_id, box_id, initial_y_position
-    if list_of_display:
-        canvas.delete("label", "box", "game_over")  # Delete previous label, box, and game over message
-        # Create a rectangle box around the text
-        box_id = canvas.create_rectangle(50, 70, 250, 100, fill="skyblue", tags="box")
-        # Calculate the center of the box
-        box_center_x = (50 + 250) / 2
-        box_center_y = (70 + 100) / 2
-        # Create the text at the center of the box
-        label_id = canvas.create_text(box_center_x, box_center_y, text=list_of_display[0], font=("Arial", 12), anchor='center', tags="label")
-        initial_y_position = 70  # Reset initial y-position
-        animate_label(label_id, box_id, initial_y_position)  # Start animation
-        entry.bind("<Return>", handle_input)
+
+    if not list_of_answers:  # Reset lists when there are no more questions
+        list_of_display.extend(["a", "b", "c", "d"])
+        list_of_answers.extend(["1", "2", "3", "4"])
+
+    canvas.delete("label", "box", "game_over")  # Delete previous label, box, and game over message
+    # Create a rectangle box around the text
+    box_id = canvas.create_rectangle(50, 70, 250, 100, fill="skyblue", tags="box")
+    # Calculate the center of the box
+    box_center_x = (50 + 250) / 2
+    box_center_y = (70 + 100) / 2
+    # Create the text at the center of the box
+    label_id = canvas.create_text(box_center_x, box_center_y, text=list_of_display[0], font=("Arial", 12), anchor='center', tags="label")
+    initial_y_position = 70  # Reset initial y-position
+    animate_label(label_id, box_id, initial_y_position)  # Start animation
+    entry.bind("<Return>", handle_input)
 
 # Function to animate the label and box falling down
 def animate_label(label_id, box_id, y_position):
     global initial_y_position
-    bbox = canvas.bbox(label_id)  # Get the bounding box of the label
-    
-    if not list_of_display: # There is no more list-elements left:
+
+    ''' For not endless-mode
+    if not list_of_display:  # There is no more list-elements left:
         # Display Congratulations message
         canvas.create_text(150, 225, text="You won!", font=("Arial", 20), fill="green", tags="win")
-        entry.destroy() # Remove user-text-box
+        entry.destroy()  # Remove user-text-box
         canvas.delete("label", "box")  # Delete previous label and box
         if questions == 0:
             canvas.create_text(150, 300, text="You answered 0 questions", font=("Arial", 12), anchor='center', tags="label")
         elif questions < 0:
             canvas.create_text(150, 300, text=("You answered " + str(questions) + " questions"), font=("Arial", 12), anchor='center', tags="label")
-
+    '''
+            
+    bbox = canvas.bbox(label_id)  # Get the bounding box of the label
 
     if bbox is not None:  # Check if bbox is not None
         label_height = bbox[3] - bbox[1]  # Calculate the height of the label text
-        
-        if y_position + label_height < 480:  
+
+        if y_position + label_height < 480:
             canvas.move(label_id, 0, movement_speed)
             canvas.move(box_id, 0, movement_speed)
             window.after(20, animate_label, label_id, box_id, y_position + movement_speed)
@@ -86,12 +92,15 @@ def animate_label(label_id, box_id, y_position):
             # Remove the entry widget when the game is over
             entry.destroy()
             point_label.destroy()
+            window.geometry("350x560")
             canvas.delete("label", "box")  # Delete previous label and box
             if questions == 0:
-                canvas.create_text(150, 300, text="You did not answer any questions", font=("Arial", 12), anchor='center', tags="label")
-            elif questions < 0:
+                canvas.create_text(150, 300, text=("You did not answer any questions"), font=("Arial", 12), anchor='center', tags="label")
+            if questions == 1:
+                canvas.create_text(150, 300, text=("You answered one question"), font=("Arial", 12), anchor='center', tags="label")
+            if questions > 1:
                 canvas.create_text(150, 300, text=("You answered " + str(questions) + " questions"), font=("Arial", 12), anchor='center', tags="label")
-    
+
 # User Input
 def handle_input(event):
     global questions  # Declare questions as a global variable
